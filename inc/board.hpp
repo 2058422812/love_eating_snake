76ebb4c32a1545ca8d444868e26a2b4b0814565a
AAAA <1458038993@qq.com>
2019-09-18 21:56:04 +0800

    board.cpp updaate 1

---
+++ inc/board.hpp
@@ -0,0 +1,20 @@
+#include "opencv2/core.hpp"
+#include "Snake.hpp"
+using namespace cv;
+
+class board
+{
+private:
+    int size;
+    Mat image;
+    int fruit[];
+    Snake snake;
+    void initMat();
+
+public:
+    board(/* args */);
+    void refresh();
+    void create_fruit();
+
+};
+
--- src/board.cpp
+++ src/board.cpp
@@ -1,21 +1,14 @@
 #include "opencv2/core.hpp"
 #include "Snake.hpp"
+#include "board.hpp"
 using namespace cv;
 
-class board
-{
-private:
-    int size;
-    Mat image;
-    int fruit[];
-    Snake snake;
-
-public:
-    board(/* args */);
-    void refresh();
-    void create_fruit();
-};
 
 board::board(/* args */)
 {
 }
+void board::initMat()
+{
+    
+}
+
