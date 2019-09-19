ed90c75e43c7c911c91bfb13945b25ece1c89cae
BabyXinORZ <1635639396@qq.com>
2019-09-19 20:29:42 +0800

    move a square

--- inc/board.hpp
+++ inc/board.hpp
@@ -8,7 +8,6 @@ class board
 private:
     int static sideLength;
     int static width;
-    Mat image;
     // int fruit[];
     // Snake snake;
     void initMat();
@@ -20,12 +19,21 @@ private:
 public:
     board(int s);
     int size;
+    Mat image;
+    static Scalar red;
+    static Scalar white;
+    static Scalar black;
+    void DrawSquare(int pos[], Scalar color);
 
     // void refresh();
     // void create_fruit();
 };
 int board::width = 5;
 int board::sideLength = 40;
+Scalar board::red = Scalar(0, 0, 255);
+Scalar board::white = Scalar(255, 255, 255);
+Scalar board::black = Scalar(0, 0, 0);
+
 board::board(int s)
 {
     size = s;
@@ -35,24 +43,27 @@ void board::DrawLine(Mat img, Point start, Point end)
 {
     int t = width;
     int l = 8;
-    line(img,
-         start,
-         end,
-         Scalar(255, 255, 255),
-         t,
-         l);
+    rectangle(image, start, end, white, -1);
 }
+
 void board::HorizonalLine()
 {
-    for (int i = 0; i < size + 2; i++)
+    for (int i = 0; i < size + 1; i++)
     {
-        DrawLine(image, Point(0, (i - 1) * (sideLength + width) + 1), Point(matSideLen, (i - 1) * (sideLength + width) + 1)); /* code */
+        DrawLine(image, Point(0, (i) * (sideLength + width)), Point(matSideLen, (i) * (sideLength + width) + width)); /* code */
     }
 }
 void board::VerticalLine()
 {
-    for (int i = 0; i < size + 2; i++)
+    for (int i = 0; i < size + 1; i++)
     {
-        DrawLine(image, Point((i - 1) * (sideLength + width) + 1, 0), Point((i - 1) * (sideLength + width) + 1, matSideLen)); /* code */
+        DrawLine(image, Point((i) * (sideLength + width), 0), Point((i) * (sideLength + width) + width, matSideLen)); /* code */
     }
+}
+
+void board::DrawSquare(int pos[], Scalar color)
+{
+    int row_val = width + pos[1] * (width + sideLength);
+    int col_val = width + pos[0] * (width + sideLength);
+    rectangle(image, Point(row_val, col_val), Point(row_val + sideLength, col_val + sideLength), color, -1);
 }
--- src/board.cpp
+++ src/board.cpp
@@ -11,6 +11,43 @@ using namespace std;
 int main()
 {
     board board1(20);
+
+    int pos[] = {10, 10};
+    board1.DrawSquare(pos, board::red);
+
+    imshow("1", board1.image);
+    while (true)
+    {
+        char key = waitKey(1);
+        switch (key)
+        {
+        case 'w':
+            board1.DrawSquare(pos, board::black);
+            pos[0] = pos[0] - 1;
+            board1.DrawSquare(pos, board::red);
+            break;
+        case 's':
+            board1.DrawSquare(pos, board::black);
+            pos[0] = pos[0] + 1;
+            board1.DrawSquare(pos, board::red);
+            break;
+        case 'a':
+            board1.DrawSquare(pos, board::black);
+            pos[1] = pos[1] - 1;
+            board1.DrawSquare(pos, board::red);
+            break;
+        case 'd':
+            board1.DrawSquare(pos, board::black);
+            pos[1] = pos[1] + 1;
+            board1.DrawSquare(pos, board::red);
+            break;
+
+        default:
+            break;
+        }
+        imshow("1", board1.image);
+    }
+
     return 0;
 }
 
@@ -18,9 +55,7 @@ void board::initMat()
 {
     matSideLen = size * sideLength + (size + 1) * width;
     cout << matSideLen;
-    image = Mat::zeros(matSideLen, matSideLen, CV_32F);
+    image = Mat::zeros(matSideLen, matSideLen, CV_8UC3);
     HorizonalLine();
     VerticalLine();
-    imshow("1", image);
-    waitKey(0);
 }
