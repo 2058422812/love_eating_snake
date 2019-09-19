9feda317c3df1b6119ab975db7a509973a3b66c1
BabyXinORZ <1635639396@qq.com>
2019-09-19 18:58:12 +0800

    move something from board.cpp to board.hpp

--- inc/board.hpp
+++ inc/board.hpp
@@ -1,4 +1,5 @@
 #include "opencv2/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 #include "Snake.hpp"
 using namespace cv;
 
@@ -25,3 +26,33 @@ public:
 };
 int board::width = 5;
 int board::sideLength = 40;
+board::board(int s)
+{
+    size = s;
+    initMat();
+}
+void board::DrawLine(Mat img, Point start, Point end)
+{
+    int t = width;
+    int l = 8;
+    line(img,
+         start,
+         end,
+         Scalar(255, 255, 255),
+         t,
+         l);
+}
+void board::HorizonalLine()
+{
+    for (int i = 0; i < size + 2; i++)
+    {
+        DrawLine(image, Point(0, (i - 1) * (sideLength + width) + 1), Point(matSideLen, (i - 1) * (sideLength + width) + 1)); /* code */
+    }
+}
+void board::VerticalLine()
+{
+    for (int i = 0; i < size + 2; i++)
+    {
+        DrawLine(image, Point((i - 1) * (sideLength + width) + 1, 0), Point((i - 1) * (sideLength + width) + 1, matSideLen)); /* code */
+    }
+}
--- src/board.cpp
+++ src/board.cpp
@@ -8,11 +8,12 @@
 using namespace cv;
 using namespace std;
 
-board::board(int s)
+int main()
 {
-    size = s;
-    initMat();
+    board board1(20);
+    return 0;
 }
+
 void board::initMat()
 {
     matSideLen = size * sideLength + (size + 1) * width;
@@ -20,39 +21,6 @@ void board::initMat()
     image = Mat::zeros(matSideLen, matSideLen, CV_32F);
     HorizonalLine();
     VerticalLine();
-
     imshow("1", image);
     waitKey(0);
-}
-
-int main()
-{
-    board board1(20);
-    return 0;
-}
-void board::DrawLine(Mat img, Point start, Point end)
-{
-    int t = width;
-    int l = 8;
-    line(img,
-         start,
-         end,
-         Scalar(255, 255, 255),
-         t,
-         l);
-}
-void board::HorizonalLine()
-{
-    for (int i = 0; i < size + 2; i++)
-    {
-        cout << i;
-        DrawLine(image, Point(0, (i - 1) * (sideLength + width)+1), Point(matSideLen, (i - 1) * (sideLength + width)+1)); /* code */
-    }
-}
-void board::VerticalLine()
-{
-    for (int i = 0; i < size + 2; i++)
-    {
-        DrawLine(image, Point((i - 1) * (sideLength + width)+1, 0), Point((i - 1) * (sideLength + width)+1, matSideLen)); /* code */
-    }
 }
