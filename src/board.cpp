f41de9c28b6d11e43fce8176b0099eba7390d323
Nixiaoxie <644950013@qq.com>
2019-09-20 21:54:12 +0800

    move fuitEat

--- src/board.cpp
+++ src/board.cpp
@@ -8,48 +8,48 @@
 using namespace cv;
 using namespace std;
 
-// int main()
-// {
-//     board board1(20);
+int main()
+{
+    board board1(20);
 
-//     int pos[] = {1, 3};
-//     board1.DrawSquare(pos, board::red);
+    int pos[] = {1, 3};
+    board1.DrawSquare(pos, board::red);
 
-//     imshow("1", board1.image);
-//     while (true)
-//     {
-//         char key = waitKey(1);
-//         switch (key)
-//         {
-//         case 'w':
-//             board1.DrawSquare(pos, board::black);
-//             pos[0] = pos[0] - 1;
-//             board1.DrawSquare(pos, board::red);
-//             break;
-//         case 's':
-//             board1.DrawSquare(pos, board::black);
-//             pos[0] = pos[0] + 1;
-//             board1.DrawSquare(pos, board::red);
-//             break;
-//         case 'a':
-//             board1.DrawSquare(pos, board::black);
-//             pos[1] = pos[1] - 1;
-//             board1.DrawSquare(pos, board::red);
-//             break;
-//         case 'd':
-//             board1.DrawSquare(pos, board::black);
-//             pos[1] = pos[1] + 1;
-//             board1.DrawSquare(pos, board::red);
-//             break;
+    imshow("1", board1.image);
+    while (true)
+    {
+        char key = waitKey(1);
+        switch (key)
+        {
+        case 'w':
+            board1.DrawSquare(pos, board::black);
+            pos[0] = pos[0] - 1;
+            board1.DrawSquare(pos, board::red);
+            break;
+        case 's':
+            board1.DrawSquare(pos, board::black);
+            pos[0] = pos[0] + 1;
+            board1.DrawSquare(pos, board::red);
+            break;
+        case 'a':
+            board1.DrawSquare(pos, board::black);
+            pos[1] = pos[1] - 1;
+            board1.DrawSquare(pos, board::red);
+            break;
+        case 'd':
+            board1.DrawSquare(pos, board::black);
+            pos[1] = pos[1] + 1;
+            board1.DrawSquare(pos, board::red);
+            break;
 
-//         default:
-//             break;
-//         }
-//         imshow("1", board1.image);
-//     }
+        default:
+            break;
+        }
+        imshow("1", board1.image);
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
 void board::initMat()
 {
